1a2cba1da5cdff7f45bff34fd8ef0d2d6e477442
irys36@hotmail.com <irys36@hotmail.com>
2022-09-27 23:33:35 +0200

    javascript

---
+++ javascript/basic_algorithmss/iteracion3.js
@@ -0,0 +1,30 @@
+// Iteración #3: Operadores
+
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+let multiplicar = 10 * 5;
+console.log(multiplicar);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+let dividir = 10 / 2;
+console.log(dividir);
+
+// 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+let resto = 15 % 9;
+console.log(resto);
+
+// 1.4 Usa el correcto operador de asignación que resultará en o = 15, 
+// teniendo dos variables p = 10 y j = 5.
+
+let p=10, j=5;
+let suma = p + j;
+console.log(suma);
+
+// 1.5 Usa el correcto operador de asignación que resultará en i = 50,
+// teniendo dos variables c = 10 y m = 5.
+
+let c=10, m=5;
+let multiplicar1 = c * m;
+console.log(multiplicar1);
---
+++ javascript/basic_algorithmss/iteracion4.js
@@ -0,0 +1,30 @@
+//Iteración #4: Arrays
+
+/* 1.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.*/
+
+/* const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+ */
+
+/* 1.2 Cambia el primer elemento de avengers a "IRONMAN"*/
+/* const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"]; 
+avengers[0] = "IRONMAN";
+console.log(avengers); */
+
+
+
+/* 1.3 console numero de elementos en el array usando la propiedad correcta de Array.
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"]; 
+console.log(avengers.length);*/
+
+/* 1.4 Añade 2 elementos al array: "Morty" y "Summer". 
+Muestra en consola el último personaje del array */
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+
+/* //TODAVIA NO
+1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+
+// TODAVIA NO
+1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"]; */ 
