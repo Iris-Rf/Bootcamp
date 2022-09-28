070f8504bf922368dca9263042ed8f56a3ad8619
irys36@hotmail.com <irys36@hotmail.com>
2022-09-28 19:08:19 +0200

    javascript

--- javascript/basic_algorithmss/iteracion4.js
+++ javascript/basic_algorithmss/iteracion4.js
@@ -19,12 +19,24 @@ console.log(avengers.length);*/
 
 /* 1.4 Añade 2 elementos al array: "Morty" y "Summer". 
 Muestra en consola el último personaje del array */
-const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+/* const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters);
+ */
+
 
 /* //TODAVIA NO
-1.5 Elimina el último elemento del array y muestra el primero y el último por consola.
-const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+1.5 Elimina el último elemento del array y muestra el primero y el último por consola.*/ 
+/* const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.pop();
+console.log(rickAndMortyCharacters);
+console.log([rickAndMortyCharacters[0], rickAndMortyCharacters[rickAndMortyCharacters.length - 1]]); 
+console.log([rickAndMortyCharacters[0], rickAndMortyCharacters[4]]);  */
+ 
+
+
 
 // TODAVIA NO
-1.6 Elimina el segundo elemento del array y muestra el array por consola.
-const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"]; */ 
+/* 1.6 Elimina el segundo elemento del array y muestra el array por consola.
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];  
+console.log(rickAndMortyCharacters.splice(1));*/
