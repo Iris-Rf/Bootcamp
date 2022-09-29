f90cdaacce7d34e5b0c7c3b2cdc3ada8d70f7bdb
irys36@hotmail.com <irys36@hotmail.com>
2022-09-29 22:50:41 +0200

    javascript

--- javascript/functions/iteracion3.js
+++ javascript/functions/iteracion3.js
@@ -3,6 +3,7 @@ let sumNumbers = 0;
 
 function sumAll(param) {
   for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] != NaN) {
     sumNumbers += numbers[i];
   }
   return sumNumbers;
---
+++ javascript/functions/iteracion5.js
@@ -0,0 +1,14 @@
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+let siEsNum = 0;
+
+function sumNum(param) {
+  for (let i = 0; i < param.length; i++) {
+siEsNum += numbers[i];
+}
+return siEsNum;
+}
+
+
+console.log(sumNum(siEsNum));
+
+// A resolver
