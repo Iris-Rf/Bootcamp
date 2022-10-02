e4c9824c0778a1f2120ae4fa471489d665435ed6
irys36@hotmail.com <irys36@hotmail.com>
2022-10-02 22:51:54 +0200

    javascript

--- javascript/dom/iteracion2/iteracion2.js
+++ javascript/dom/iteracion2/iteracion2.js
@@ -1,50 +1,50 @@
 // 2.1 Inserta dinamicamente en un html un div vacio con javascript.
 
-let divNode = document.querySelector("div");
+const divNode = document.querySelector("div");
 console.log(divNode);
 
-let newDiv = document.createElement("div");
+const newDiv = document.createElement("div");
 newDiv.appendChild(divNode);
 
 // 2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.
 
-let divNode1 = document.querySelector("div");
+const divNode1 = document.querySelector("div");
 
-let newDiv1 = document.createElement("div");
+const newDiv1 = document.createElement("div");
 newDiv1.appendChild(divNode1);
 
-let newP = document.createElement("p");
+const newP = document.createElement("p");
 newDiv1.appendChild(newP);
 console.log(newDiv1);
 
 
 // 2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
 
-let divNode2 = document.querySelector("div");
+const divNode2 = document.querySelector("div");
 
-let newDiv2 = document.createElement("div");
+const newDiv2 = document.createElement("div");
 newDiv2.appendChild(divNode2);
 
 for (i = 0; i <= 5; i++) {
-  let newP1 = document.createElement("p");
+  const newP1 = document.createElement("p");
   newDiv2.appendChild(newP1);
 }
 console.log(newDiv2);
 
 // 2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
 
-let divNode3 = document.querySelector("p");
+const divNode3 = document.querySelector("p");
 
-let newP2 = document.createElement("p");
+const newP2 = document.createElement("p");
 
-let newP2Text = document.createTextNode("Soy dinámico!"); 
+const newP2Text = document.createTextNode("Soy dinámico!"); 
 newP2.appendChild(newP2Text);
 console.log(newP2);
 
 // 2.5 Inserta en el h2 con la clase .fn-insert-here el texto 'Wubba Lubba dub dub'.
 
-let h2Node = document.querySelector('.fn-insert-here');
-let h2ChangeText = document.createTextNode('Wubba Lubba dub dub');
+const h2Node = document.querySelector('.fn-insert-here');
+const h2ChangeText = document.createTextNode('Wubba Lubba dub dub');
 
 h2Node.appendChild(h2ChangeText);
 console.log(h2Node);
@@ -53,11 +53,22 @@ console.log(h2Node);
 // 2.6 Basandote en el siguiente array crea una lista ul > li con los textos del array.
 // const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
 
-let listNodes = document.querySelector("ul");
-let listUl = document.createElement('ul');
-console.log(listUl);
-// let textList = document.createTextNode(['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter']);
-// listNodes.appendChild(textList);
+
+const listNodes = document.querySelector("ul");
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+const list = document.createElement('ul');
+
+for (const app of apps) {
+ 
+  const li = `<li>${app}</li>`;
+  list.innerHTML += li;
+}
+
+console.log(list);
+
+
+// const listText = document.createTextNode(['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter']);
+// listNodes.appendChild(nodes);
 // console.log(listNodes);
 
 
