13b726e35bda7e550924adb36f77a4cf054f9bca
irys36@hotmail.com <irys36@hotmail.com>
2022-10-02 18:00:45 +0200

    javascript

--- javascript/dom/iteracion2/iteracion2.js
+++ javascript/dom/iteracion2/iteracion2.js
@@ -1,23 +1,38 @@
 // 2.1 Inserta dinamicamente en un html un div vacio con javascript.
 
-let divNode = document.querySelector('div');
+let divNode = document.querySelector("div");
 console.log(divNode);
 
 let newDiv = document.createElement("div");
-newDiv.appendChild(divNode); 
+newDiv.appendChild(divNode);
 
 // 2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.
 
-let divNode1 = document.querySelector('div');
+let divNode1 = document.querySelector("div");
 
 let newDiv1 = document.createElement("div");
-newDiv1.appendChild(divNode1); 
+newDiv1.appendChild(divNode1);
 
 let newP = document.createElement("p");
 newDiv1.appendChild(newP);
 console.log(newDiv1);
 
+
 // 2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
+
+let divNode2 = document.querySelector("div");
+
+let newDiv2 = document.createElement("div");
+newDiv1.appendChild(divNode2);
+
+let newP1 = document.createElement("p");
+newDiv1.appendChild(newP1);
+console.log(newDiv2);
+
+for (i = 0; i <= 5; i++) {
+  let newP1 = document.createElement("p");
+  newDiv2.appendChild(newP1);
+}
 
 // let newP6 = document.createElement("p");
 // newDiv2.appendChild(newP6);
@@ -32,7 +47,7 @@ console.log(newDiv1);
 
 // 2.7 Elimina todos los nodos que tengan la clase .fn-remove-me
 
-// 2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div. 
+// 2.8 Inserta una p con el texto 'Voy en medio!' entre los dos div.
 // 	Recuerda que no solo puedes insertar elementos con .appendChild.
 
 // 2.9 Inserta p con el texto 'Voy dentro!', dentro de todos los div con la clase .fn-insert-here
