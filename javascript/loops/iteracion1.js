eb4b974948e1305a3d8172097cf0b110f8e9b6d5
unknown <irys36@hotmail.com>
2022-10-04 22:27:50 +0200

    javascript

--- javascript/loops/iteracion1.js
+++ javascript/loops/iteracion1.js
@@ -1,18 +1,12 @@
-/* **Iteración #1: Usa includes**
-
-Haz un bucle y muestra por consola todos aquellos valores del array que incluyan la palabra
- "Camiseta". Usa la función .***includes*** de javascript. */
-
- const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta',
-  'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
-
+const products = [
+  "Camiseta de Pokemon",
+  "Pantalón coquinero",
+  "Gorra de gansta",
+  "Camiseta de Basket",
+  "Cinrurón de Orión",
+  "AC/DC Camiseta",
+];
 
 for (const product of products) {
-    
-    if (products.includes == "Camiseta"){
-        console.log(product)
-         }
-         return product;
+  if (product.includes("Camiseta")) console.log(product);
 }
-
-console.log(products(product));
---
+++ javascript/loops/iteracion4.js
@@ -0,0 +1,15 @@
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+
+for (const key in alien) {
+    if (Object.hasOwnProperty.call(alien, key)) {
+        const element = alien[key];
+        
+    }
+}
+
+console.log(alien);
