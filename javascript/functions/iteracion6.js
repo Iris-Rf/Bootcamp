d2f9493722a26e3e1b4a21bc615c66ecb516291d
Iris-Rf <irys@hotmail.com>
2022-10-14 23:44:07 +0200

    javascript

--- javascript/functions/iteracion6.js
+++ javascript/functions/iteracion6.js
@@ -1,26 +1,26 @@
 const duplicates = [
-    'sushi',
-    'pizza',
-    'burger',
-    'potatoe',
-    'pasta',
-    'ice-cream',
-    'pizza',
-    'chicken',
-    'onion rings',
-    'pasta',
-    'soda'
-  ];
-  function removeDuplicates(duplicates) {
-  let result = duplicates.reduce;
+  "sushi",
+  "pizza",
+  "burger",
+  "potatoe",
+  "pasta",
+  "ice-cream",
+  "pizza",
+  "chicken",
+  "onion rings",
+  "pasta",
+  "soda",
+];
+
+let duplicate = [];
+function removeDuplicates(duplicates) {
+  
   for (let i = 0; i < duplicates.length; i++) {
-    const element = duplicates[i];
+    if (duplicates[i + 1] == duplicates[i]) {
+      duplicate.push(duplicates[i]);
+    }
+    return duplicate;
   }
-    if (result.includes(duplicates)) {
-        duplicates.push(duplicates);
-  }
-  return result;
 }
 
-console.log(result); 
-
+console.log(removeDuplicates);
---
+++ javascript/functions/iteracion8.js
@@ -0,0 +1,21 @@
+const counterWords = [
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "upgrade",
+  "code",
+];
+let totalCount = [];
+const repeatCounter = (elements) => {
+  elements.forEach((element) => {
+    totalCount[element] = totalCount[element] + 1 || 1;
+  });
+  return totalCount;
+};
+console.log(repeatCounter(counterWords));
